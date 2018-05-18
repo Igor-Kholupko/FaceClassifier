1e630c5ea87d670ba29f3f83f3256347df042186
Julie Avel <juli.avelchuk@gmail.com>
2018-05-18 17:15:01 +0300

    Login page
    Adding login page without user models.

--- FaceClassifier/settings.py
+++ FaceClassifier/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'login.apps.LoginConfig',
     'directories',
 ]
 
@@ -129,4 +130,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
