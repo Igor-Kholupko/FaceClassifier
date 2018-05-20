a389883fe8ef2b40b0fd5f4f340de061969f9d8e
Julie Avel <juli.avelchuk@gmail.com>
2018-05-20 22:41:18 +0300

    Add cash

--- workspace/views.py
+++ workspace/views.py
@@ -17,11 +17,11 @@ def workspace(request):
         main_form.dir_forms = list()
     if main_form.dir_forms.__len__() == 0:
         for directory in Directory.objects.using('directories').filter(is_busy='0')[:10]:
-            if directory.is_busy == '0':
-                directory.is_busy = ("%d" % cur_user.id)
-            else:
-                directory.is_busy += (",%d" % cur_user.id)
-            directory.save(using='directories')
+            #if directory.is_busy == '0':
+            #    directory.is_busy = ("%d" % cur_user.id)
+            #else:
+            #    directory.is_busy += (",%d" % cur_user.id)
+            #directory.save(using='directories')
             dir_form = DirectoryForm(data=directory)
             dir_form.item_forms = list()
             for item in DirectoryItem.objects.using('directories').filter(dir_id=directory):
@@ -29,7 +29,7 @@ def workspace(request):
                 dir_form.item_forms.append(item_form)
             main_form.dir_forms.append(dir_form)
     if request.method == "POST":
-        main_form.data = main_form.cleaned_data
+        #main_form.data = main_form.cleaned_data
         print(request.POST)
     dictionary = dict()
     dictionary.update(main_form=locals()['main_form'])
